04da5551472faf99a4ac2d99d9f0c158f0183c8a
samipourquoi <sami.guiderdoni@gmail.com>
2021-07-23 18:10:47 +0200

    add multi command parsign

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    let result = parser::parse(r#"
+        /say hello world
+        /help
+        /tellraw @a [{"text":"hello world"},{"score":{"name":"@s","objective":"deaths"}}]
+
+        if 1 == 2 {
+
+        }
+    "#);
+    dbg!(result);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,33 @@
 use nom::{
+    bytes::complete::take_until,
     IResult,
-    multi::{many0},
-    combinator::{opt},
-    bytes::complete::{tag},
-    character::complete::{anychar}
+    multi::many0,
+    combinator::opt,
+    bytes::complete::tag,
+    character::{
+        complete::{
+            anychar,
+            multispace0
+        },
+        complete::line_ending as eol
+    },
+    branch::alt,
+    sequence::{terminated, delimited},
+    combinator::eof,
+    multi::many1,
+    character::complete::{alphanumeric1, alphanumeric0}
 };
+use nom::character::complete::not_line_ending;
+
+#[derive(Debug)]
+pub struct AST {
+    statements: Vec<Statement>
+}
+
+#[derive(Debug)]
+enum Statement {
+    Command(Command)
+}
 
 #[derive(Debug, PartialEq)]
 struct Command {
@@ -13,23 +36,37 @@ struct Command {
 
 type ParseResult<'a, T> = IResult<&'a str, T>;
 
-fn parse_command(input: &str) -> ParseResult<Command> {
-    let (input, _) = opt(tag("/"))(input)?;
-    let (input, command) = many0(anychar)(input)?;
-    let value: String = command.iter().collect();
+pub fn parse(input: &str) -> ParseResult<AST> {
+    let (input, statements) = many0(
+        delimited(multispace0, parse_statement, multispace0)
+    )(input)?;
 
-    Ok((input, Command { value }))
+    Ok((input, AST {
+        statements
+    }))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn parse_statement(input: &str) -> ParseResult<Statement> {
+    let (input, command) = parse_command(input)?;
+    Ok((input, Statement::Command(command)))
+}
 
-    #[test]
-    fn command() {
-        let result = parse_command("/say hello world");
-        assert_eq!(result, Ok(("", Command {
-            value: "say hello world".to_string()
-        })));
+fn parse_command(input: &str) -> ParseResult<Command> {
+    let (input, _) = tag("/")(input)?;
+    let (input, command) = read_line(input)?;
+
+    Ok((input, Command { value: command.to_string() }))
+}
+
+fn read_line(input: &str) -> ParseResult<String> {
+    let (input, line) = terminated(not_line_ending, eol)(input)?;
+    Ok((input, line.to_string()))
+}
+
+fn end_of_line(input: &str) -> ParseResult<&str> {
+    if input.is_empty() {
+        Ok((input, input))
+    } else {
+        eol(input)
     }
 }
